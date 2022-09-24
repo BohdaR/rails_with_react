6d13db9c6357d124f8e0b02dd0c4fb1fa4547642
BohdaR <bohdanshushval@gmail.com>
2022-09-24 12:24:27 +0300

    created offices controller

---
+++ app/controllers/offices_controller.rb
@@ -0,0 +1,48 @@
+class OfficesController < ApplicationController
+  before_action :set_office, only: [:show, :update, :destroy]
+
+  def index
+    @offices = Office.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @office = Room.new
+  end
+
+  def create
+    @office = Room.new(room_params)
+    if @office.save
+      render json: @office
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @office.update(room_params)
+      redirect_to @office
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @office.destroy
+    redirect_to offices_url
+  end
+
+  private
+  def set_office
+    @office = Office.find(params[:id])
+  end
+
+  def room_params
+    params.require(:office).permit(:company_id, :street, :house_number, :town, :province, :country)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :reservations
+  resources :offices
   resources :rooms do
     resources :places
   end
-  root 'home#index'
   devise_for :user,
      controllers: {
        omniauth_callbacks: 'users/omniauth_callbacks'
