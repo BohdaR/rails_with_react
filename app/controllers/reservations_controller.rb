5f04259929930e2e1124ec5e46ec764db82c51ed
BohdaR <bohdanshushval@gmail.com>
2022-10-12 20:34:24 +0300

    reservations index action has been edited

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,8 +4,18 @@ class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :update, :destroy]
 
   def index
-    @reservations = Reservation.where(employee: get_employee)
-    render json: @reservations
+    reservations =
+      Reservation
+        .joins("INNER JOIN places on places.id = reservations.place_id")
+        .joins("INNER JOIN rooms on rooms.id = places.room_id")
+        .select("
+        reservations.id,
+        rooms.name as room_name,
+        places.number as place_number,
+        reservations.start_at,
+        reservations.end_at")
+        .where(employee: get_employee)
+    render json: reservations
   end
 
   def show
