a3af4da270097dc097aa1a3762d7b93d3bb0bb58
savka-olia <tokarchykolia@gmail.com>
2022-10-19 12:17:30 +0300

    rubocop correcting

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :places
   end
   resources :favorites, only: [:index, :create, :destroy]
-  
+
   devise_for :user,
      controllers: {
        omniauth_callbacks: "users/omniauth_callbacks"
