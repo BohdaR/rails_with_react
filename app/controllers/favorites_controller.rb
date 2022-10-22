5bb92b004d403ff44bb289d2030681d09024f6c4
BohdaR <bohdanshushval@gmail.com>
2022-10-22 12:12:03 +0300

    rubocop

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,35 +1,33 @@
 # frozen_string_literal: true
 
 class FavoritesController < ApplicationController
-	before_action :set_place, only: [:create, :destroy]
+  before_action :set_place, only: [:create, :destroy]
 
-	def index
+  def index
     favorites = Favorite.where(employee_id: @employee.id)
-		render_json: favorites
+    render json: favorites
   end
 
-	def create
+  def create
     favorite = get_employee.favorites.new(place_id: @place.id)
     if favorite.save
-			render json: favorite
-		else
-			render json: favorite.errors.full_messages, status: 422
-		end
-		
-	end
+      render json: favorite
+    else
+      render json: favorite.errors.full_messages, status: 422
+    end
+  end
 
-	def destroy
+  def destroy
     favorite = get_employee.favorites.find_by(place_id: @place.id)
     render json: favorite.destroy if favorite.present?
-	end
+  end
 
-	private 
+  private
+    def get_employee
+      current_user.employee
+      end
 
-	def get_employee
-			current_user.employee 
-		end 
-
-		def set_place
-			@place = Place.find(params[:place_id])
-		end
+    def set_place
+      @place = Place.find(params[:place_id])
+    end
 end
