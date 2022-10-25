fb07762742717185fcb0b8e52be0c54c263992d4
BohdaR <bohdanshushval@gmail.com>
2022-10-25 12:38:22 +0300

    styled sign up form

--- app/javascript/components/RegForm.jsx
+++ app/javascript/components/RegForm.jsx
@@ -4,21 +4,33 @@ import GoogleLoginButton from "./GoogleLoginButton";
 import PasswordInput from "./Inputs/PasswordInput";
 import AuthenticityTokenInput from "./Inputs/AuthenticityTokenInput";
 import EmailInput from "./Inputs/EmailInput";
-import {authentication} from '../stylesheets/authentication_form.module.css'
+import {
+  authentication,
+  authenticationHeadline,
+  authenticationLinks,
+  formContainer, formSeparator, signupWrapper
+} from '../stylesheets/authentication_form.module.css'
 
 const RegForm = ({form_authenticity_token}) => {
   return (
-    <div className={authentication}>
-      <h1>Sing up</h1>
-      <form action="/user" id="new_user" method="post">
-        <AuthenticityTokenInput token={form_authenticity_token}/>
-        <EmailInput name="user[email]"/>
-        <PasswordInput placeholder="Password" name="user[password]"/>
-        <PasswordInput placeholder="Password confirmation" name="user[password_confirmation]"/>
-        <SubmitInput value="Sign up"/>
-      </form>
-      <div className="authentication-links">
-        <GoogleLoginButton form_authenticity_token={form_authenticity_token}/>
+    <div className={formContainer}>
+      <div className={authentication}>
+        <h1 className={authenticationHeadline}>Sing up</h1>
+        <form action="/user" id="new_user" method="post">
+          <AuthenticityTokenInput token={form_authenticity_token}/>
+          <EmailInput name="user[email]"/>
+          <PasswordInput placeholder="Password" name="user[password]"/>
+          <PasswordInput placeholder="Password confirmation" name="user[password_confirmation]"/>
+          <SubmitInput value="Sign up"/>
+        </form>
+        <div className={formSeparator}>OR</div>
+        <div className={authenticationLinks}>
+          <GoogleLoginButton form_authenticity_token={form_authenticity_token}/>
+        </div>
+        <div className={signupWrapper}>
+          Already have an account?
+          <a className="signin-links signin-links-text" href="/user/sign_up">Log in</a>
+        </div>
       </div>
     </div>
   );
