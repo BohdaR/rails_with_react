7a96dad4335d146665168ec20ff395e1c72717b2
BohdaR <bohdanshushval@gmail.com>
2022-11-07 13:25:26 +0200

    fix time bug

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,7 +7,7 @@ class Place < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  def self.free_places(look_from = Time.now, look_to = Time.now + 1.day)
+  def self.free_places(look_from = Time.zone.now, look_to = Time.zone.now + 1.day)
     Place.left_joins(:reservations)
          .where("place_id IS NULL")
          .or(Reservation.where("end_at < ?", look_from).where("? < ?", look_from, look_to))
