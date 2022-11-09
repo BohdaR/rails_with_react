e7a5cf7fd9c3fd4bfdfd7200d9412621174d728e
savka-olia <tokarchykolia@gmail.com>
2022-11-09 18:23:56 +0200

    add email field

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,4 +6,6 @@ class Employee < ApplicationRecord
   belongs_to :office
   has_many :reservations, dependent: :delete_all
   has_many :favorites, dependent: :delete_all
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_124314) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_142114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,11 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_124314) do
   end
 
   create_table "employees", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.bigint "company_id", null: false
     t.bigint "office_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.index ["company_id"], name: "index_employees_on_company_id"
     t.index ["office_id"], name: "index_employees_on_office_id"
     t.index ["user_id"], name: "index_employees_on_user_id"
