70b131b7369b2c5643a0b0da68c10aa765cfdcde
BohdaR <bohdanshushval@gmail.com>
2022-11-17 12:27:13 +0200

    added company admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,25 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user&.roles.empty?
-      can :read, :all # allow everyone to read everything
-      can :manage, :rails_admin
-    else
-      can :access, :rails_admin # only allow admin users to access Rails Admin
-      can :manage, :all
+    # can [:read], :dashboard
+    # can [:read, :update], Reservation, employee_id: user.employee.id
+    # can :manage, :rails_admin
+
+    user.roles.each do |role|
+      case role.name
+      when "superadmin"
+        can :access, :rails_admin # only allow admin users to access Rails Admin
+        can :manage, :all
+      when "company admin"
+        can :access, :rails_admin
+        can :manage, :dashboard
+
+        can :manage, [User, Reservation, Favorite], employee: { company: user.employee.company }
+        can [:read, :update, :destroy], Company, id: user.employee.company_id
+        can :manage, Employee, company_id: user.employee.company_id
+        can :manage, [Office, Room], company: user.employee.company
+        can :manage, Place, room: { company: user.employee.company }
+      end
     end
   end
 end
