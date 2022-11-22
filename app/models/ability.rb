85b909f58ef9528a03d6790a87ab5e891e2d5e11
BohdaR <bohdanshushval@gmail.com>
2022-11-22 12:13:15 +0200

    added grant_permissions method

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,5 +8,51 @@ class Ability
     employee = user.employee
 
     return unless employee
+
+    employee.roles.each do |role|
+      permissions = role.permissions
+      permissions.each do |permission|
+        grant_permissions(permission.name)
+        case permission.name
+        # manage whatever
+        when "manage all"
+          can :manage, :all
+
+        # access to rails admin
+        when "have access to rails admin"
+          can :access, :rails_admin
+          can :manage, :dashboard
+
+        when "manage the company"
+          can :read, [Role, Permission]
+          can :read, [User, Reservation, Favorite], employee: { company: employee.company }
+          can [:read, :update, :destroy], Company, id: employee.company_id
+          can :manage, Employee, company_id: employee.company_id
+          can :manage, [Office, Room], company: employee.company
+          can :manage, Place, room: { company: employee.company }
+
+        when "manage company employees information"
+          can :manage, Employee, id: employee.id
+          can :manage, [User, Reservation, Favorite], employee:
+
+        when "read information about the company"
+          can :read, Company, id: employee.company_id
+          can :read, [Office, Room], company: employee.company
+          can :read, Place, room: { company: employee.company }
+        end
+      end
+    end
   end
+
+  private
+    def grant_permissions(permission)
+      return if permission.split.length != 3
+
+      action, _, model_name = permission.split
+      model_index = ApplicationRecord.descendants.as_json.index(model_name.singularize.capitalize)
+
+      if model_index
+        can action.to_sym, ApplicationRecord.descendants[model_index]
+      end
+    end
 end
