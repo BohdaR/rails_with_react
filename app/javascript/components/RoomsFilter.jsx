4f08a078fe88a6b10adf53ac7604a59e506985c7
savka-olia <tokarchykolia@gmail.com>
2022-12-02 10:59:38 +0200

    add stepper in fe side

--- app/javascript/components/RoomsFilter.jsx
+++ app/javascript/components/RoomsFilter.jsx
@@ -3,8 +3,7 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import StepContent from '@mui/material/StepContent';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
+import { makeStyles } from '@mui/styles';
 import Button from '@mui/material/Button';
 import {
   roomsFilter,
@@ -31,12 +30,18 @@ const RoomsFilter = ({
   const [floor, setFloor] = useState(defaultFloor);
   const [activeStep, setActiveStep] = useState(0);
 
-  const steps = [
-    {label: "Select an office"},
-    {label: "Select an floor"},
-    {label: "Pick a start date"},
-    {label: "Pick an end date"}
-  ];
+  const useStyles = makeStyles(() => ({
+    root: {
+      "& .Mui-active .MuiStepIcon-root": { color: "#173166" },
+      "& .Mui-completed .MuiStepIcon-root": { color: "#173166" },
+      "& .MuiStepLabel-label": { fontSize: "18px", color: "#173166" },
+      "& .MuiButton-contained": { background: "#173166" },
+      "& .MuiButton-contained:hover": { background: "#94a412" },
+      "& .MuiStepIcon-text": { fontSize: "15px"}
+    }
+  }));
+
+  const classes = useStyles();
   
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -46,115 +51,150 @@ const RoomsFilter = ({
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   }
 
-  const handleReset = () => {
-    setActiveStep(0);
-  }
-
   return (
-    <div>
-    <div>
-      <Stepper orientation="vertical" activeStep={activeStep}>
-        {steps.map((step, index) => (
-          <Step key={step.label}>
-            <StepLabel
-              optional={
-                index === 3 ? (
-                  <Typography variant="caption">Last step</Typography>
-                ) : null
-            }>
-              {step.label}
-            </StepLabel>
-            <StepContent>
-            <div>
-                  <Button
-                    variant="contained"
-                    onClick={handleNext}
-                    sx={{ mt: 1, mr: 1 }}
-                  >
-                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
-                  </Button>
-                  <Button
-                    disabled={index === 0}
-                    onClick={handleBack}
-                    sx={{ mt: 1, mr: 1 }}
-                  >
-                    Back
-                  </Button>
-                </div>
-            </StepContent>
-          </Step>
-        ))}
-      </Stepper>
-      {activeStep === steps.length && (
-        <Paper square elevation={0} sx={{ p: 3 }}>
-          <Typography>All steps completed - you&apos;re finished</Typography>
-          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
-            Reset
-          </Button>
-        </Paper>
-      )}
-    </div>
+  <div>
     <div className={roomsFilter}>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <div className={wrapper}>
-          <label className={bookingLabel}>Pick an office
-            <select className={officePicker} name="office_id" value={office} onChange={(e) => {
-              onChangeOfficeId(e.target.value)
-              setOffice(e.target.value)
-            }}>
-              {offices.map(office =>
-                <option
-                  value={office.id}
-                  key={office.id}>
-                  {`${office.town}, ${office.street}, ${office.house_number}`}
-                </option>
-              )}
-            </select>
-          </label>
-        </div>
-        <div className={wrapper}>
-          <label className={bookingLabel}>Pick a floor
-            <select className={officePicker} name="floor" value={floor} onChange={(e) => {
-              onChangeFloor(e.target.value)
-              setFloor(e.target.value)
-            }}>
-              {floors.map(floor =>
-                <option
-                  value={floor.floor}
-                  key={floor.floor}>
-                  {floor.floor}
-                </option>
-              )}
-            </select>
-          </label>
-        </div>
-        <div className={wrapper}>
-          <label className={bookingLabel}>Pick a start date
-            <input
-              className={bookingInput}
-              type="datetime-local"
-              value={lookFromTime}
-              min={lookFromTime}
-              name="look_from"
-              onChange={(e) => {
-                onChangeLookFromTime(e.target.value);
-              }}/>
-          </label>
-        </div>
-        <div className={wrapper}>
-          <label className={bookingLabel}>Pick an end date
-            <input
-              className={bookingInput}
-              type="datetime-local"
-              value={lookToTime}
-              min={lookFromTime}
-              name="look_to"
-              onChange={(e) => {
-                onChangeLookToTime(e.target.value);
-              }}/>
-          </label>
-        </div>
-      </form>
+      <Stepper orientation="vertical" activeStep={activeStep} className={classes.root}>
+        <Step>
+          <StepLabel>Select an office </StepLabel>
+          <StepContent>
+            <div className={wrapper}>
+              <label className={bookingLabel}>Pick an office
+                <select className={officePicker} name="office_id" value={office} onChange={(e) => {
+                  onChangeOfficeId(e.target.value)
+                  setOffice(e.target.value)
+                }}>
+                  {offices.map(office =>
+                    <option
+                      value={office.id}
+                      key={office.id}>
+                      {`${office.town}, ${office.street}, ${office.house_number}`}
+                    </option>
+                  )}
+                </select>
+              </label>
+            </div>
+          <div>
+            <Button
+              variant="contained"
+              onClick={handleNext}
+              sx={{ mt: 1, mr: 1 }}
+              size="small"
+            >
+              Continue
+            </Button>
+              </div>
+          </StepContent>
+        </Step>
+        <Step>
+          <StepLabel>Select a floor</StepLabel>
+          <StepContent>
+            <div className={wrapper}>
+              <label className={bookingLabel}>Pick a floor
+                <select className={officePicker} name="floor" value={floor} onChange={(e) => {
+                  onChangeFloor(e.target.value)
+                  setFloor(e.target.value)
+                }}>
+                  {floors.map(floor =>
+                    <option
+                      value={floor.floor}
+                      key={floor.floor}>
+                      {floor.floor}
+                    </option>
+                  )}
+                </select>
+              </label>
+            </div>
+            <div>
+              <Button
+                variant="contained"
+                onClick={handleNext}
+                size="small"
+                sx={{ mt: 1, mr: 1 }}
+              >
+              Continue
+              </Button>
+              <Button
+                onClick={handleBack}
+                size="small"
+                sx={{ mt: 1, mr: 1 }}
+              >
+                Back
+              </Button>
+            </div>
+          </StepContent>
+        </Step>
+        <Step>
+          <StepLabel>Pick a start date</StepLabel>
+          <StepContent>
+            <div className={wrapper}>
+              <label className={bookingLabel}>Pick a start date
+                <input
+                  className={bookingInput}
+                  type="datetime-local"
+                  value={lookFromTime}
+                  min={lookFromTime}
+                  name="look_from"
+                  onChange={(e) => {
+                    onChangeLookFromTime(e.target.value);
+                  }}/>
+              </label>
+            </div>
+            <div>
+              <Button
+                variant="contained"
+                onClick={handleNext}
+                size="small"
+                sx={{ mt: 1, mr: 1 }}
+              >
+              Continue
+              </Button>
+              <Button
+                onClick={handleBack}
+                size="small"
+                sx={{ mt: 1, mr: 1 }}
+              >
+                Back
+              </Button>
+            </div>
+          </StepContent>
+        </Step>
+        <Step>
+          <StepLabel>Pick an end date</StepLabel>
+          <StepContent>
+            <div className={wrapper}>
+              <label className={bookingLabel}>Pick an end date
+                <input
+                  className={bookingInput}
+                  type="datetime-local"
+                  value={lookToTime}
+                  min={lookFromTime}
+                  name="look_to"
+                  onChange={(e) => {
+                    onChangeLookToTime(e.target.value);
+                  }}/>
+              </label>
+            </div>
+            <div>
+              <Button
+                variant="contained"
+                onClick={handleNext}
+                sx={{ mt: 1, mr: 1 }}
+                size="small"
+              >
+                Finish
+              </Button>
+              <Button
+                onClick={handleBack}
+                sx={{ mt: 1, mr: 1 }}
+                size="small"
+              >
+                Back
+              </Button>
+            </div>
+          </StepContent>
+        </Step>
+      </Stepper>
     </div>
   </div>
   );
