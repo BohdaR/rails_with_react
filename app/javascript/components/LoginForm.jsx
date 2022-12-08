8c764d7c598b53e723f59afb1fd1ca9098beac5e
BohdaR <bohdanshushval@gmail.com>
2022-12-08 14:06:56 +0200

    removed redirect if server respond 401

--- app/javascript/components/LoginForm.jsx
+++ app/javascript/components/LoginForm.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import GoogleLoginButton from "./GoogleLoginButton";
 import PasswordInput from "./Inputs/PasswordInput";
 import AuthenticityTokenInput from "./Inputs/AuthenticityTokenInput";
 import EmailInput from "./Inputs/EmailInput";
 import SubmitInput from "./Inputs/SubmitInput";
-import {authentication,
+import {
+  authentication,
   formContainer,
   authenticationLinks,
   checkboxLabel,
@@ -14,28 +15,73 @@ import {authentication,
   forgotPasswordLink,
   signupWrapper
 } from '../stylesheets/authentication_form.module.css'
+import {get, post} from "./useAPI/useAPI";
 
 const LoginForm = ({form_authenticity_token}) => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [rememberMe, setRememberMe] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      authenticity_token: form_authenticity_token,
+      user: {
+        password: password,
+        email: email,
+        remember_me: rememberMe
+      }
+    }
+
+    const config = {
+      headers: {
+        accept: 'application/json'
+      }
+    }
+
+    post(`${process.env.HOST}/user/sign_in`, data, config).then(
+      (responce) => {
+        document.location.replace('/')
+      }
+    ).catch(
+      (errors) => {
+        console.error(errors['response']);
+      }
+    )
+  }
+
   return (
     <div className={formContainer}>
       <div className={authentication}>
         <h1 className={authenticationHeadline}>Log in</h1>
-        <form action="/user/sign_in" id="new_user" method="post">
+        <form action="/user/sign_in" id="new_user" method="post" onSubmit={(e) => handleSubmit(e)}>
           <AuthenticityTokenInput token={form_authenticity_token}/>
-          <EmailInput name="user[email]"/>
-          <PasswordInput placeholder="Password" name="user[password]"/>
-          <input className={checkbox} type="checkbox" value="1" name="user[remember_me]" id="user_remember_me"/>
+          <EmailInput name="user[email]" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <PasswordInput
+            placeholder="Password"
+            value={password}
+            name="user[password]"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            className={checkbox}
+            type="checkbox"
+            value={rememberMe}
+            name="user[remember_me]"
+            id="user_remember_me"
+            onChange={(e) => setRememberMe(e.target.value)}
+          />
           <label className={checkboxLabel} htmlFor="user_remember_me">Remember me</label>
           <SubmitInput value="Log in"/>
         </form>
         <div className={formSeparator}>OR</div>
         <div className={authenticationLinks}>
-            <GoogleLoginButton form_authenticity_token={form_authenticity_token}/>
+          <GoogleLoginButton form_authenticity_token={form_authenticity_token}/>
           <a href="/user/password/new" className={forgotPasswordLink}>Forgot your password?</a>
         </div>
         <div className={signupWrapper}>
           Don't have an account?
-          <a class="signin-links signin-links-text" href="/user/sign_up">Sign Up</a>
+          <a className="signin-links signin-links-text" href="/user/sign_up">Sign Up</a>
         </div>
       </div>
     </div>
--- app/javascript/components/useAPI/useAPI.js
+++ app/javascript/components/useAPI/useAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export const get = async (url) => {
-  return await axios.get(url);
+export const get = async (url, config) => {
+  return await axios.get(url, config);
 }
 
-export const post = async (url, body) => {
-  return await axios.post(url, body);
+export const post = async (url, body, config) => {
+  return await axios.post(url, body, config);
 }
 
-export const deleteRequest = async (url) => {
-  return await axios.delete(url);
+export const deleteRequest = async (url, config) => {
+  return await axios.delete(url, config);
 }
