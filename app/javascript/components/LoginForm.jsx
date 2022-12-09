b84edd26abbf990f6ca987ba4e02aaa679274e17
BohdaR <bohdanshushval@gmail.com>
2022-12-09 14:39:47 +0200

    added error alerts to sing in and sing up pages

--- app/javascript/components/LoginForm.jsx
+++ app/javascript/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {Fragment, useState} from 'react';
 import GoogleLoginButton from "./GoogleLoginButton";
 import PasswordInput from "./Inputs/PasswordInput";
 import AuthenticityTokenInput from "./Inputs/AuthenticityTokenInput";
@@ -16,9 +16,11 @@ import {
   signupWrapper
 } from '../stylesheets/authentication_form.module.css'
 import {get, post} from "./useAPI/useAPI";
+import {Alert} from "@mui/material";
 
 const LoginForm = ({form_authenticity_token}) => {
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
   const [email, setEmail] = useState('');
   const [rememberMe, setRememberMe] = useState('');
   const handleSubmit = (e) => {
@@ -45,46 +47,51 @@ const LoginForm = ({form_authenticity_token}) => {
       }
     ).catch(
       (errors) => {
-        console.error(errors['response']);
+        setErrors(errors.response.data.error)
       }
     )
   }
 
   return (
-    <div className={formContainer}>
-      <div className={authentication}>
-        <h1 className={authenticationHeadline}>Log in</h1>
-        <form action="/user/sign_in" id="new_user" method="post" onSubmit={(e) => handleSubmit(e)}>
-          <AuthenticityTokenInput token={form_authenticity_token}/>
-          <EmailInput name="user[email]" value={email} onChange={(e) => setEmail(e.target.value)}/>
-          <PasswordInput
-            placeholder="Password"
-            value={password}
-            name="user[password]"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            className={checkbox}
-            type="checkbox"
-            value={rememberMe}
-            name="user[remember_me]"
-            id="user_remember_me"
-            onChange={(e) => setRememberMe(e.target.value)}
-          />
-          <label className={checkboxLabel} htmlFor="user_remember_me">Remember me</label>
-          <SubmitInput value="Log in"/>
-        </form>
-        <div className={formSeparator}>OR</div>
-        <div className={authenticationLinks}>
-          <GoogleLoginButton form_authenticity_token={form_authenticity_token}/>
-          <a href="/user/password/new" className={forgotPasswordLink}>Forgot your password?</a>
-        </div>
-        <div className={signupWrapper}>
-          Don't have an account?
-          <a className="signin-links signin-links-text" href="/user/sign_up">Sign Up</a>
+      <div className={formContainer}>
+        <div className={authentication}>
+          <h1 className={authenticationHeadline}>Log in</h1>
+          {errors ?
+          <Alert severity="error" onClose={() => {setErrors(null)}} style={{marginBottom: 10}}>
+            {errors}
+          </Alert> : null
+          }
+          <form action="/user/sign_in" id="new_user" method="post" onSubmit={(e) => handleSubmit(e)}>
+            <AuthenticityTokenInput token={form_authenticity_token}/>
+            <EmailInput name="user[email]" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <PasswordInput
+              placeholder="Password"
+              value={password}
+              name="user[password]"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              className={checkbox}
+              type="checkbox"
+              value={rememberMe}
+              name="user[remember_me]"
+              id="user_remember_me"
+              onChange={(e) => setRememberMe(e.target.value)}
+            />
+            <label className={checkboxLabel} htmlFor="user_remember_me">Remember me</label>
+            <SubmitInput value="Log in"/>
+          </form>
+          <div className={formSeparator}>OR</div>
+          <div className={authenticationLinks}>
+            <GoogleLoginButton form_authenticity_token={form_authenticity_token}/>
+            <a href="/user/password/new" className={forgotPasswordLink}>Forgot your password?</a>
+          </div>
+          <div className={signupWrapper}>
+            Don't have an account?
+            <a className="signin-links signin-links-text" href="/user/sign_up">Sign Up</a>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- app/javascript/components/RegForm.jsx
+++ app/javascript/components/RegForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SubmitInput from "./Inputs/SubmitInput";
 import GoogleLoginButton from "./GoogleLoginButton";
 import PasswordInput from "./Inputs/PasswordInput";
@@ -10,17 +10,81 @@ import {
   authenticationLinks,
   formContainer, formSeparator, signupWrapper
 } from '../stylesheets/authentication_form.module.css'
+import {post} from "./useAPI/useAPI";
+import {Alert} from "@mui/material";
 
 const RegForm = ({form_authenticity_token}) => {
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [errors, setErrors] = useState({});
+  const [email, setEmail] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      authenticity_token: form_authenticity_token,
+      user: {
+        password: password,
+        password_confirmation: passwordConfirmation,
+        email: email,
+      }
+    }
+
+    const config = {
+      headers: {
+        accept: 'application/json'
+      }
+    }
+
+    post(`${process.env.HOST}/user`, data, config).then(
+      (responce) => {
+        document.location.replace('/')
+      }
+    ).catch(
+      (errors) => {
+        setErrors(errors.response.data.errors)
+      }
+    )
+  }
   return (
     <div className={formContainer}>
       <div className={authentication}>
         <h1 className={authenticationHeadline}>Sing up</h1>
-        <form action="/user" id="new_user" method="post">
-          <AuthenticityTokenInput token={form_authenticity_token}/>
-          <EmailInput name="user[email]"/>
-          <PasswordInput placeholder="Password" name="user[password]"/>
-          <PasswordInput placeholder="Password confirmation" name="user[password_confirmation]"/>
+        {errors.email ?
+          <Alert severity="error" onClose={() => {
+            setErrors({email: null, password: errors.password, password_confirmation: errors.password_confirmation})
+          }} style={{marginBottom: 10}}>
+            Email {errors.email} <br/>
+          </Alert> : null
+        }
+        {errors.password ?
+          <Alert severity="error" onClose={() => {
+            setErrors({email: errors.email, password: null, password_confirmation: errors.password_confirmation})
+          }} style={{marginBottom: 10}}>
+            Password {errors.password}
+          </Alert> : null
+        }
+        {errors.password_confirmation ?
+          <Alert severity="error" onClose={() => {
+            setErrors({email: errors.email, password: errors.password, password_confirmation: null})
+          }} style={{marginBottom: 10}}>
+            Password confirmation {errors.password_confirmation}
+          </Alert> : null
+        }
+        <form action="/user" id="new_user" method="post" onSubmit={(e) => handleSubmit(e)}>
+          <EmailInput name="user[email]" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <PasswordInput
+            placeholder="Password"
+            value={password}
+            name="user[password]"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <PasswordInput
+            placeholder="Password confirmation"
+            value={passwordConfirmation}
+            name="user[password_confirmation]"
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
           <SubmitInput value="Sing up"/>
         </form>
         <div className={formSeparator}>OR</div>
