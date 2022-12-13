ddcdb11221f965d0d194e8362a572d78a5858b94
savka-olia <tokarchykolia@gmail.com>
2022-12-13 14:09:27 +0200

    change favorite buttons to icons

--- app/javascript/components/Reservation.jsx
+++ app/javascript/components/Reservation.jsx
@@ -2,12 +2,15 @@ import React, {useEffect, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import style from '../stylesheets/reservations.module.css'
-import {deleteRequest, post} from "./useAPI/useAPI";
+import { deleteRequest, post, get } from "./useAPI/useAPI";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import "../stylesheets/favorite_places.css";
 
 
 const Reservation = ({reservation, token}) => {
   const [showReservation, setShowReservation] = useState(true);
+  const [favorites, setFavorites] = useState([]);
   const [favorited, setFavorited] = useState(false);
 
   const dateHandler = (dateString) => {
@@ -15,11 +18,20 @@ const Reservation = ({reservation, token}) => {
     return `${date.toDateString()} ${date.toTimeString().substring(0, 5)}`
   }
 
+  useEffect(() => {
+    get(`${process.env.HOST}/favorites`)
+      .then(
+        (response) => {
+          console.log(response.data)
+          setFavorites(response.data);
+        })
+  }, []);
+
   const deleteReservation = (reservation_id) => {
     deleteRequest(`${process.env.HOST}/reservations/${reservation_id}?authenticity_token=${token}`)
       .then(
         (response) => {
-          setShowReservation(false)
+          setShowReservation(false);
         }
       )
   }
@@ -34,8 +46,9 @@ const Reservation = ({reservation, token}) => {
 			}).then(
 			(response) => {
         console.log(response.data);
-				setFavorited(prev => !prev);
-        alert( `Place ${reservation.place_number} successfully added to favorites.`)
+        setFavorited(prev => !prev);
+        console.log(favorited);
+        alert( `Place ${reservation.place_number} successfully added to favorites.`);
 			});
     }
 	}
@@ -56,6 +69,18 @@ const Reservation = ({reservation, token}) => {
           {dateHandler(reservation.end_at)}
         </div>
         <div className={`${style.btnColumn} ${style.col}`}>
+          {!favorited ? (
+            <FavoriteBorderIcon
+              onClick={onfavoritedClick}
+              style={{
+                color: "#173166"
+            }}/>
+          ) : (
+            <FavoriteIcon 
+              style={{
+              color: "#173166"
+            }}/>
+          )} 
           <IconButton onClick={() => deleteReservation(reservation.id)}>
             <DeleteIcon style={{
               color: "#173166"
--- db/schema.rb
+++ db/schema.rb
@@ -42,27 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_091433) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "allowed_actions", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "allowed_actions_permissions", id: false, force: :cascade do |t|
-    t.bigint "allowed_action_id", null: false
-    t.bigint "permission_id", null: false
-    t.index ["allowed_action_id"], name: "index_allowed_actions_permissions_on_allowed_action_id"
-    t.index ["permission_id"], name: "index_allowed_actions_permissions_on_permission_id"
-  end
-
-  create_table "auth_groups", force: :cascade do |t|
-    t.string "name"
-    t.integer "priority"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -82,13 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_091433) do
     t.index ["company_id"], name: "index_employees_on_company_id"
     t.index ["office_id"], name: "index_employees_on_office_id"
     t.index ["user_id"], name: "index_employees_on_user_id"
-  end
-
-  create_table "employees_roles", id: false, force: :cascade do |t|
-    t.bigint "employee_id", null: false
-    t.bigint "role_id", null: false
-    t.index ["employee_id"], name: "index_employees_roles_on_employee_id"
-    t.index ["role_id"], name: "index_employees_roles_on_role_id"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -134,25 +106,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_091433) do
     t.index ["company_id"], name: "index_offices_on_company_id"
   end
 
-  create_table "permissions", force: :cascade do |t|
-    t.string "name"
-    t.bigint "auth_group_id", null: false
-    t.bigint "scope_id", null: false
-    t.bigint "subject_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["auth_group_id"], name: "index_permissions_on_auth_group_id"
-    t.index ["scope_id"], name: "index_permissions_on_scope_id"
-    t.index ["subject_id"], name: "index_permissions_on_subject_id"
-  end
-
-  create_table "permissions_roles", id: false, force: :cascade do |t|
-    t.bigint "role_id", null: false
-    t.bigint "permission_id", null: false
-    t.index ["permission_id"], name: "index_permissions_roles_on_permission_id"
-    t.index ["role_id"], name: "index_permissions_roles_on_role_id"
-  end
-
   create_table "places", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.integer "number"
@@ -168,16 +121,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_091433) do
     t.datetime "end_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "calendar_id"
+    t.string "calendar_link"
     t.index ["employee_id"], name: "index_reservations_on_employee_id"
     t.index ["place_id"], name: "index_reservations_on_place_id"
-  end
-
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.bigint "auth_group_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["auth_group_id"], name: "index_roles_on_auth_group_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -189,21 +136,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_091433) do
     t.bigint "company_id", null: false
     t.index ["company_id"], name: "index_rooms_on_company_id"
     t.index ["office_id"], name: "index_rooms_on_office_id"
-  end
-
-  create_table "scopes", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "priority", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "subjects", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -218,6 +150,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_091433) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "user"
+    t.string "access_token"
+    t.datetime "expires_at"
+    t.string "refresh_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -231,13 +167,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_091433) do
   add_foreign_key "favorites", "places"
   add_foreign_key "issues", "companies"
   add_foreign_key "offices", "companies"
-  add_foreign_key "permissions", "auth_groups"
-  add_foreign_key "permissions", "scopes"
-  add_foreign_key "permissions", "subjects"
   add_foreign_key "places", "rooms"
   add_foreign_key "reservations", "employees"
   add_foreign_key "reservations", "places"
-  add_foreign_key "roles", "auth_groups"
   add_foreign_key "rooms", "companies"
   add_foreign_key "rooms", "offices"
 end
