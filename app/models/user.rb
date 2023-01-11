aa71b0b3bbe30451c2c420f276ebe9be381f73f9
BohdaR <bohdanshushval@gmail.com>
2023-01-11 17:29:55 +0200

    Revert "Revert "fixed issue sing in issue""
    This reverts commit 0cd2dc501e8d31c08fd93fd0d794b6a39122156a.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,15 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_google(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.full_name = auth.info.name
-      user.avatar_url = auth.info.image
+    if where(email: auth.info.email).first
+      where(email: auth.info.email).first
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.full_name = auth.info.name
+        user.avatar_url = auth.info.image
+      end
     end
   end
 
